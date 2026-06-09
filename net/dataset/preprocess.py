b49def366095529c0fae287781ec0e961d90613f
yusuftengriverdi <yusufbtanriverdi@hotmail.com>
2026-06-09 16:26:46 +0300

    gtpp0

--- net/dataset/preprocess.py
+++ net/dataset/preprocess.py
@@ -32,156 +32,154 @@ def perform_preprocessing(dataframe, params, logger=None):
     ct = 0
     ct_not_found = 0
     dataframe.loc[:, 'csvfound'] = True
-    # Iterate over each file for processing
-    for i, filename in tqdm(enumerate(filenames), total=len(filenames)):
-        # Prepare file paths and identifiers from dataframe row
-        image_path = os.path.join(dataframe.loc[i, 'fscan'])
-        name = dataframe.loc[i, 'osid']
 
-        save_im_path = params.dataset_.sys + params.preprocessing.save_dir + '/' + dataframe.loc[i, 'mscan']
-        save_csv_path = params.dataset_.sys + params.preprocessing.save_dir + '/' + dataframe.loc[i, 'mcsv']
-        save_lmk_path = params.dataset_.sys + params.preprocessing.save_dir + '/' + dataframe.loc[i, 'mlmk']
+    if params.preprocessing_.gtpp:
+        gtpp(params.preprocessing_.gtpp)
+    else: # Usual routine
+        # Iterate over each file for processing
+        for i, filename in tqdm(enumerate(filenames), total=len(filenames)):
+            # Prepare file paths and identifiers from dataframe row
+            image_path = os.path.join(dataframe.loc[i, 'fscan'])
+            name = dataframe.loc[i, 'osid']
+            
+            save_im_path = params.dataset_.sys + params.preprocessing.save_dir + '/' + dataframe.loc[i, 'mscan']
+            save_csv_path = params.dataset_.sys + params.preprocessing.save_dir + '/' + dataframe.loc[i, 'mcsv']
+            save_lmk_path = params.dataset_.sys + params.preprocessing.save_dir + '/' + dataframe.loc[i, 'mlmk']
 
-        # Skip processing if image already exists
-        if os.path.exists(save_im_path):
-            # logger.info('Image seems to be processed already!!!')
-            # logger.info(filename)
-            continue
+            # Skip processing if image already exists
+            if os.path.exists(save_im_path):
+                # logger.info('Image seems to be processed already!!!')
+                # logger.info(filename)
+                continue
 
-        # Load ultrasound image volume and header metadata
-        V, header = extract_image(image_path)
-        V = np.array(V)
+            # Load ultrasound image volume and header metadata
+            V, header = extract_image(image_path)
+            V = np.array(V)
 
-        lmk_path = dataframe.loc[i, '_fcsv']
+            lmk_path = dataframe.loc[i, '_fcsv']
 
-        # Attempt to load landmarks CSV file
-        try:
-            lmk = pd.read_csv(lmk_path)
-        except Exception as e:
-            logger.error(str(e))
-            ct_not_found += 1
-            logger.warning(f"Could not load landmarks from {lmk_path} for {name}! Skipping!! Count: {ct_not_found}")
-            dataframe.loc[i, 'csvfound'] = False
-            continue
+            # Attempt to load landmarks CSV file
+            try:
+                lmk = pd.read_csv(lmk_path)
+            except Exception as e:
+                logger.error(str(e))
+                ct_not_found += 1
+                logger.warning(f"Could not load landmarks from {lmk_path} for {name}! Skipping!! Count: {ct_not_found}")
+                dataframe.loc[i, 'csvfound'] = False
+                continue
 
-        # Verify landmarks count
-        if len(lmk) != 19:
-            logger.warning('Something wrong!! Landmark count mismatch: %s', filename)
+            # Verify landmarks count
+            if len(lmk) != 19:
+                logger.warning('Something wrong!! Landmark count mismatch: %s', filename)
 
-        # Convert landmark points to matrix format in millimeters
-        L_in_mm = get_matrix_of_lmks(lmk)
+            # Convert landmark points to matrix format in millimeters
+            L_in_mm = get_matrix_of_lmks(lmk)
 
-        # Log header info for debugging
-        # logger.info(header)
+            # Log header info for debugging
+            # logger.info(header)
 
-        # Determine pixel spacing information from header; fallback if keys missing
-        try:
-            p = header.get('spacings')[:3]
-        except Exception as e:
-            p = np.array([header['space directions'][0, 0],
-                          header['space directions'][1, 1],
-                          header['space directions'][2, 2]])
-
-        img_size = np.array(V.shape)
-
-        # --------- #
-        # STEP 1: Apply low-pass filter to the 3D ultrasound image volume
-        # --------- #
-        if params.preprocessing_.filter:
-            V = filter_3d_image(V, 
-                                params.preprocessing_.params.filter.filter_size, 
-                                params.preprocessing_.params.filter.reflect ) 
-
-        # ----------  #
-        # STEP 2: Interpolate image to desired spacing and size
-        # ----------  #
-
-        if params.preprocessing_.bspline:
-            image = sitk.GetImageFromArray(V)
-            image.SetSpacing(p)  # Set original spacing
-
-            new_spacing = params.preprocessing_.params.bspline.spacing_
-            new_size = params.preprocessing_.params.bspline.size_
-
-            # Configure the resampler filter for image interpolation
-            resampler = sitk.ResampleImageFilter()
-            resampler.SetOutputSpacing(new_spacing)
-            resampler.SetSize(new_size)
-            resampler.SetInterpolator(sitk.sitkBSpline)
-            resampler.SetOutputDirection(image.GetDirection())
-            resampler.SetOutputOrigin(image.GetOrigin())
-
-            resampled_image = resampler.Execute(image)
-            V = sitk.GetArrayFromImage(resampled_image)
-            header['sizes'] = params.preprocessing_.params.bspline.size_
-
-            p = new_spacing
             # Determine pixel spacing information from header; fallback if keys missing
             try:
-                header['spacings'] = p
-            except Exception:
-                header['space directions'] = np.array([ [p[0], 0, 0],
-                                                        [0, p[1], 0],
-                                                        [0, 0, p[2]]
-                                                        ]
-                                                    )
-                
-        # Convert landmarks from mm to pixel units
-        L_in_pix = L_in_mm / p
+                p = header.get('spacings')[:3]
+            except Exception as e:
+                p = np.array([header['space directions'][0, 0],
+                            header['space directions'][1, 1],
+                            header['space directions'][2, 2]])
 
-        if params.preprocessing_.swap:
-            # Reorient image and landmarks as per processing assumptions
-            V = np.transpose(V, (2, 1, 0))
-            L_in_pix = swap_xz_coordinates(L_in_pix)
+            img_size = np.array(V.shape)
 
-        # ------ -------------- #
-        # STEP 5: Apply affine transformation to image and landmarks
-        # ------ -------------- #
-        if params.preprocessing_.affine:
-            # Retrieve ground truth plane data from dictionary for current image
-            plane = dicto.get(name)
-            if plane is None:
-                logger.warning(f"Ground truth for {name} not found!")
-                # ct_not_found += 1
-                dataframe.loc[i, 'rot_found'] = False
-                continue
-            ct += 1
-            # Assumption: Centers are in RAS coordinate system
-            center_gt_in_pix = np.array([-1 * plane["center"][0], -1 * plane["center"][1], plane["center"][2]]) / p
-            img_size = np.array(V.shape).astype(np.float32)
-            # Compute translation vector for affine transform
-            translation_vector = (center_gt_in_pix - img_size / 2.0) * (2.0 / img_size).astype(np.float32)
-            # Compute rotation matrix from plane parameters
-            rotation_3x3 = affine3Dmatrix(plane)
+            # --------- #
+            # STEP 1: Apply low-pass filter to the 3D ultrasound image volume
+            # --------- #
+            if params.preprocessing_.filter:
+                V = filter_3d_image(V, 
+                                    params.preprocessing_.params.filter.filter_size, 
+                                    params.preprocessing_.params.filter.reflect ) 
 
-            # Construct full 3x4 affine transformation matrix
-            transform_matrix = np.zeros((3, 4))
-            transform_matrix[:3, :3] = rotation_3x3
-            transform_matrix[:3, 3] = translation_vector
+            # ----------  #
+            # STEP 2: Interpolate image to desired spacing and size
+            # ----------  #
 
-            # Apply affine transformation to the image volume
-            Vhat = grid_transform_3d(V.astype(np.float32), transform_matrix.astype(np.float32))
-            
+            if params.preprocessing_.bspline:
+                image = sitk.GetImageFromArray(V)
+                image.SetSpacing(p)  # Set original spacing
+
+                new_spacing = params.preprocessing_.params.bspline.spacing_
+                new_size = params.preprocessing_.params.bspline.size_
+
+                # Configure the resampler filter for image interpolation
+                resampler = sitk.ResampleImageFilter()
+                resampler.SetOutputSpacing(new_spacing)
+                resampler.SetSize(new_size)
+                resampler.SetInterpolator(sitk.sitkBSpline)
+                resampler.SetOutputDirection(image.GetDirection())
+                resampler.SetOutputOrigin(image.GetOrigin())
+
+                resampled_image = resampler.Execute(image)
+                V = sitk.GetArrayFromImage(resampled_image)
+                header['sizes'] = params.preprocessing_.params.bspline.size_
+
+                p = new_spacing
+                # Determine pixel spacing information from header; fallback if keys missing
+                try:
+                    header['spacings'] = p
+                except Exception:
+                    header['space directions'] = np.array([ [p[0], 0, 0],
+                                                            [0, p[1], 0],
+                                                            [0, 0, p[2]]
+                                                            ]
+                                                        )
+                    
+            # Convert landmarks from mm to pixel units
+            L_in_pix = L_in_mm / p
+
             if params.preprocessing_.swap:
-                Vhat = np.transpose(Vhat, (2, 1, 0))
+                # Reorient image and landmarks as per processing assumptions
+                V = np.transpose(V, (2, 1, 0))
                 L_in_pix = swap_xz_coordinates(L_in_pix)
 
-            L_in_pix_norm = (L_in_pix - img_size / 2.0) * (2.0 / img_size).astype(np.float32)
-            inv_translation_vector = -1 * translation_vector
+            # ------ -------------- #
+            # STEP 5: Apply affine transformation to image and landmarks
+            # ------ -------------- #
+            if params.preprocessing_.affine:
+                # Retrieve ground truth plane data from dictionary for current image
+                plane = dicto.get(name)
+                if plane is None:
+                    logger.warning(f"Ground truth for {name} not found!")
+                    # ct_not_found += 1
+                    dataframe.loc[i, 'rot_found'] = False
+                    continue
+                ct += 1
+                # Assumption: Centers are in RAS coordinate system
+                center_gt_in_pix = np.array([-1 * plane["center"][0], -1 * plane["center"][1], plane["center"][2]]) / p
+                img_size = np.array(V.shape).astype(np.float32)
+                # Compute translation vector for affine transform
+                translation_vector = (center_gt_in_pix - img_size / 2.0) * (2.0 / img_size).astype(np.float32)
+                # Compute rotation matrix from plane parameters
+                rotation_3x3 = affine3Dmatrix(plane)
 
-            Lhat_in_pix_norm = affine_transform(L_in_pix_norm, np.linalg.inv(rotation_3x3), inv_translation_vector)
-            Lhat_in_pix = (Lhat_in_pix_norm / (2.0 / img_size).astype(np.float32)) + img_size / 2.0
-            V = Vhat.copy()
-            L_in_pix = Lhat_in_pix
+                # Construct full 3x4 affine transformation matrix
+                transform_matrix = np.zeros((3, 4))
+                transform_matrix[:3, :3] = rotation_3x3
+                transform_matrix[:3, 3] = translation_vector
 
-        if params.preprocessing_.gtpp:
-            Vhat, Lhat_in_pix = gtpp(V, L_in_pix)
+                # Apply affine transformation to the image volume
+                Vhat = grid_transform_3d(V.astype(np.float32), transform_matrix.astype(np.float32))
+                
+                if params.preprocessing_.swap:
+                    Vhat = np.transpose(Vhat, (2, 1, 0))
+                    L_in_pix = swap_xz_coordinates(L_in_pix)
+
+                L_in_pix_norm = (L_in_pix - img_size / 2.0) * (2.0 / img_size).astype(np.float32)
+                inv_translation_vector = -1 * translation_vector
+
+                Lhat_in_pix_norm = affine_transform(L_in_pix_norm, np.linalg.inv(rotation_3x3), inv_translation_vector)
+                Lhat_in_pix = (Lhat_in_pix_norm / (2.0 / img_size).astype(np.float32)) + img_size / 2.0
+                V = Vhat.copy()
+                L_in_pix = Lhat_in_pix
             # Save processed image and transformed landmarks
-            V = Vhat.copy()
-            L_in_pix = Lhat_in_pix
-        # Save processed image and transformed landmarks
-        save_3d_image(V, header, img_size, save_im_path)
-        save_transformed_landmarks(L_in_pix * p, lmk, save_csv_path, save_lmk_path)
+            save_3d_image(V, header, img_size, save_im_path)
+            save_transformed_landmarks(L_in_pix * p, lmk, save_csv_path, save_lmk_path)
 
-    logger.info("Number of images with missing csvs: %d", ct_not_found)
-    dataframe.to_csv('tmp.csv')
+        logger.info("Number of images with missing csvs: %d", ct_not_found)
+        dataframe.to_csv('tmp.csv')
--- net/dataset/utility/standardization.py
+++ net/dataset/utility/standardization.py
@@ -1,2 +1,247 @@
-def gtpp(V, L_in_pix):
-    pass
+# Author: Antonia Alomar (12 Jan 2024)
+#
+# Standard scan plane detection in 3D ultrasound images of fetal head
+# Network training --> Adaptation to torch spatial transform
+#
+# In this script, we use quaternions to represent rotation.
+#
+# Reference
+# Standard Plane Detection in 3D Fetal Ultrasound Using an Iterative Transformation Network
+# https://arxiv.org/abs/1806.07486
+#
+# ==============================================================================
+
+
+import torch 
+import os
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.utils as vutils
+import torchvision.transforms.functional as TF
+import nrrd
+import gc
+
+from net.dataset.utility.gtpp.dataloaders.torch_dataloader import Create_Ultrasound_loader, US_3D_dataset_fast
+from pytorch3d.transforms import matrix_to_quaternion, euler_angles_to_matrix, quaternion_to_matrix
+from net.dataset.utility.gtpp.utils.SonoNet import SonoNet
+from net.dataset.utility.gtpp.utils import slicer_to_torch_transform
+import json
+
+torch.cuda.empty_cache()
+torch.pi = torch.acos(torch.zeros(1)).item() * 2
+trans_frac = 0.0/4                            # Percentage of middle volume to sample plane centre from. (0-1)
+max_euler = [(0.0)*torch.pi,              # Maximum range to sample the three Euler angles in radians for plane orientation.
+                (0.0)*torch.pi,
+                (0.0)*torch.pi]
+
+def sample_euler_angles_fix_range(num, max_angle1=torch.pi, max_angle2=torch.pi/2.0, max_angle3=torch.pi,seed=None):
+    """Uniform random sampling of Euler angles with restricted range. Sample angles between [-max_angle1, max_angle1]
+
+    Args:
+    num: number of random samples
+    max_angle1, max_angle2, max_angle3: maximum positive angle to sample from. Possible values are [0, pi], [0, pi/2] and [0, pi]
+    Using max_angle1=pi, max_angle2=pi/2, max_angle3=pi cover the whole rotation sphere once.
+
+    Returns:
+    angles: Euler angles (Roll-Pitch-Yaw) [num, 3]
+
+    """
+    
+    if seed is not None:
+        gen = torch.torch.Generator()
+        gen.manual_seed(seed)
+    else:
+        gen= None
+    angles = torch.zeros(num,3)   
+    angle1 = 2 * max_angle1 * torch.rand(num, generator=gen) - max_angle1
+    a = torch.cos(torch.tensor(torch.pi/2.0 - max_angle2))
+    angle2 = torch.arccos((1-2*torch.rand(num, generator=gen)) * a) - torch.pi/2.0
+    angle3 = 2 * max_angle3 * torch.rand(num, generator=gen) - max_angle3
+    angles[:,0] = angle1
+    angles[:,1] = angle2
+    angles[:,2] = angle3
+    
+    return angles
+            
+def get_slices(config, image, rot, trans, factor=1):
+    desi_s = np.array(config.params.desired_size)/factor
+    batch_size = rot.shape[0]
+    
+    mat_rot = torch.zeros(batch_size, 3, 4)
+    mat_rot[:,:, :3] = rot
+    mat_rot[:,:,3] = trans
+    
+    
+    # Translate image 
+    image_i = slicer_to_torch_transform.affine_transform(image, mat_rot)
+    
+    # Center image is the standard plane 
+    c_idx = (np.array(desi_s)/2)+1
+    
+    slices = torch.zeros(batch_size,3,int(desi_s[0]),int(desi_s[1]))
+    
+    # Input slices to the network
+    slices[:,0,:,:] = image_i[:,0,:,:,int(c_idx[2])]
+    slices[:,1,:,:] = image_i[:,0,:,int(c_idx[1]),:]
+    slices[:,2,:,:] = image_i[:,0,int(c_idx[0]),:,:]
+    
+    del image, mat_rot
+    gc.collect() 
+    
+    return slices, image_i[:,0,:,:,:] 
+  
+def gtpp(config):
+    # DATALOADING AND GROUND TRUTH PLANE DEFINITION    
+    prefix= 'test'
+    print(prefix)
+    Create_Ultrasound_loader(config.file_paths.list_files.test, 
+                             config.file_paths.data_dir, 
+                             config.file_paths.label_dir, 
+                             config.file_paths.out_dir_pre_pross, 
+                             config.params.desired_size, prefix, 
+                             nSamples=5, transform=True, 
+                             downsample= config.params.downsampling_factor)
+    testset = US_3D_dataset_fast(prefix, 
+                                 config.file_paths.out_dir_pre_pross, 
+                                 scan_transform=None, nSamples=5)                                  
+                                   
+    testLoader = DataLoader(testset, batch_size=1, shuffle=False, drop_last=False)
+
+    print("TEST SET:\n")
+    print("\tTotal num of batches : {}".format(len(testLoader)))
+    print("\tTotal num of examples in data set : {}".format(len(testLoader.dataset)))
+    # Define model
+    model = SonoNet(config.params, 
+                    config.params.input_plane, 
+                    config.params.num_output_tr,
+                    config.params.num_output_rr,
+                    dropout_p = 0.25)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+    model.to(device)
+    #print((config.config.batch_size ,config.input_plane, config.box_size[0], config.box_size[1]))
+    #summary(model, (config.batch_size ,config.input_plane, config.desired_size[0], config.desired_size[1]))
+    names_planes = ["sagittal","coronal","axial"]
+    print(prefix)
+    name_out_dir = prefix + "_it_"+ str(config.test_it) + '_out/' 
+    if config.int_rad:
+        name_out_dir = 'random_ini_' + name_out_dir
+    if not os.path.isdir(config.model_dir + name_out_dir):
+        os.makedirs(config.model_dir + name_out_dir)
+        os.makedirs(config.model_dir + name_out_dir+ names_planes[0]+'/')
+        os.makedirs(config.model_dir + name_out_dir+ names_planes[1]+'/')
+        os.makedirs(config.model_dir + name_out_dir + names_planes[2]+'/')
+        print('dir created')    
+    if config.params.resume:
+        # Resume previous training
+        checkpoint = torch.load(os.path.join(config.model_dir, 
+                                             config.checkpoint_name))
+        model.load_state_dict(checkpoint['model_state_dict'])
+        epoch_number = checkpoint['epoch']
+        # loss = checkpoint['loss']
+    
+    print(' LOADED EPOCH {}:'.format(epoch_number))
+
+    count = 0 
+    print(len(testLoader))
+  
+    filenames_list = []
+    model.eval()
+    
+    allplanes={}
+    # Disable gradient computation and reduce memory consumption.
+    with torch.no_grad():
+          for filenames,images_res, slices_gt, trans_gt, rots_gt, mat_gt in testLoader:
+                name = str(filenames)
+                filenames_new = name.replace("'", "")
+                filenames_new = filenames_new.replace("(", "")
+                filenames_new = filenames_new.replace(")", "")
+                filenames_new = filenames_new.replace(",", "")   
+                if "full_resolution" in prefix:
+                    factor_d=2
+                    images = images_res[0:images_res.shape[0], 0:images_res.shape[1]:factor_d, 0:images_res.shape[2]:factor_d, 0:images_res.shape[3]:factor_d] 
+
+                else:
+                    images = images_res
+                    factor_d = 1
+                
+                filenames_list.append(filenames)
+                img_val_saggital = []
+                img_val_coronal = []
+                img_val_axial = [] 
+                # Initialize plane from which we start the search
+                batch_size = images.shape[0]
+                if config.params.seed_val is not None:
+                    gen = torch.torch.Generator()
+                    gen.manual_seed(config.params.seed_val + count)
+                else:
+                    gen= None
+                
+                if config.params.int_rad:
+                    # Random translation
+                    factor_a = np.array(config.params.desired_size).astype(np.float32)/4 
+                    tran = ((torch.rand(batch_size, 3,generator=gen) * (factor_a * trans_frac) + factor_a * (1-trans_frac) / 2.0) - ((factor_a-1) / 2.0))/factor_a
+                    
+                    # Random uniform sampling of Euler angles with restricted range
+                    euler_angles = sample_euler_angles_fix_range(batch_size, max_euler[0], max_euler[1], max_euler[2], config.params.seed_val + count)
+
+                else:
+                    euler_angles =torch.zeros(batch_size, 3)
+                    tran =torch.zeros(batch_size, 3)
+                    
+                rot =  euler_angles_to_matrix(euler_angles,"XYZ")
+                # quaternions = matrix_to_quaternion(rot)
+                slices_in, _ = get_slices(config, images,rot,tran, factor=factor_d)
+                
+                rot_in = rot
+                tran_in = tran
+                
+                ##### Cumulative transformation #####
+                               
+                R = rot.to(device)
+                T = tran.to(device)
+                for _ in range(0, config.params.test_it):
+
+                    # Make predictions for this batch
+                    ytr_es, yrr_es, _, _ = model(slices_in.to(device))
+                                       
+                    # estimate output slices/ update slices that will be the next input
+                    rot = quaternion_to_matrix(yrr_es)  
+                    #  Rotations accumulated in one go    
+                    t_current = torch.bmm(R, ytr_es.unsqueeze(2))
+                    T = T + t_current[:,:,0]
+                    R = torch.bmm(R,rot) 
+                    slices_in,_ = get_slices(config,images,R,T,factor=factor_d)
+
+                if config.params.save_jpg:   
+                    for k in range(config.params.input_plane):
+                        # Extract the grayscale tensor (assuming slices_in is already a tensor)
+                        image_tensor = slices_in[0, k, :, :]
+                        # Ensure the tensor is 2D and add a fake channel dimension for compatibility
+                        image_tensor = image_tensor.unsqueeze(0)  # Shape becomes (1, H, W)   
+                        # Normalize values to the range [0, 1]
+                        image_tensor = (image_tensor - image_tensor.min()) / (image_tensor.max() - image_tensor.min())
+                        # Rotate the image by 180 degrees
+                        rotated_image = TF.rotate(image_tensor, angle=180)
+                        save_name =  config.model_dir + name_out_dir +names_planes[k]+'/'+ filenames_new + '_' + names_planes[k]+'.jpg'
+                        # Save the grayscale image
+                        vutils.save_image(rotated_image, save_name)
+                                                         
+                slices_in,image_final = get_slices(config,images_res,R,T)
+                if config.params.save_nrrd:
+                    if config.params.int_rad:
+                        filenames_new = filenames_new + '_rand_init_'
+                        _ ,images = get_slices(config, images,rot_in,tran_in, factor=factor_d)
+    
+                    nrrd.write(config.model_dir + name_out_dir+'standarized_' + filenames_new + '.nrrd',image_final.detach().numpy(), index_order='C')
+                    
+                    nrrd.write(config.model_dir + name_out_dir +'original_' + filenames_new + '.nrrd',images.detach().numpy(), index_order='C')
+                #Dictionary
+                info={'name':filenames_new,'R':R.cpu().numpy().tolist(),'t':T.cpu().numpy().tolist()}
+                allplanes.update({filenames_new:info})
+                count = count + 1
+                del img_val_saggital, img_val_axial, img_val_coronal
+                gc.collect()
+                                  
+    with open(''.join((config.model_dir + name_out_dir + config.dic_name +"_allplanes_inference.txt")), "w") as fp:
+        json.dump(allplanes,fp) 
